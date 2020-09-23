006ea91d67b6e90e5d627fd8cd9f5f451ad3e011
MosqueraSt3 <esmosquera66@misena.edu.co>
2020-09-22 23:37:30 -0500

    resolve conflicts 2.0

--- Blood-Stream-signUp/api/index.js
+++ Blood-Stream-signUp/api/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express')
 
 const config = require('../config.js')
 const user = require('./components/user/user-routes')
@@ -12,8 +13,11 @@ app.use(bodyParser.json())
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
+const swaggerDoc = require('./swagger.json')
+
 // ROUTER
 app.use('/api/user', user)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
 app.listen(config.api.port, () => {
   // console.log('Api escuchando en el puerto ', config.api.port);
