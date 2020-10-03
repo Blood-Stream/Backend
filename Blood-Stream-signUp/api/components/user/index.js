c3aba34496ae8b9473bf280b3fe41a043eb2094f
MosqueraSt3 <esmosquera66@misena.edu.co>
2020-10-03 13:04:00 -0500

    Carlos sigue rompiendo cosas

--- Blood-Stream-signUp/api/components/user/index.js
+++ Blood-Stream-signUp/api/components/user/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-// const store = require('../../../../Blood-Stream-db/index')
-const store = require('../../../store/dummy')
+const store = require('../../../../Blood-Stream-db/index')
+/* const store = require('../../../store/dummy') */
 // const store = require('../../../store/db')
 const ctrl = require('./user-controller')
 
