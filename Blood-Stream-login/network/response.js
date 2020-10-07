f978bdb536b05f9049aa282dfda5d3a584f8b134
cargdev <ingecarlos.gutierrez@gmail.com>
2020-10-07 11:49:42 -0500

    Adding arrow fn to network

--- Blood-Stream-login/network/response.js
+++ Blood-Stream-login/network/response.js
@@ -1,12 +1,8 @@
 'use strict'
 
-exports.success = function (req, res, message, status) {
+exports.success = (req, res, message, status) => {
   const statusCode = status || 200
   const statusMessage = message || ''
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
   res.status(status).send({
     error: false,
     status: statusCode,
@@ -14,16 +10,17 @@ exports.success = function (req, res, message, status) {
   })
 }
 
-exports.error = function (req, res, message, status) {
+exports.error = (req, res, message, status) => {
   const statusCode = status || 500
   const statusMessage = message || 'Internal server error'
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
   res.status(statusCode).send({
     error: false,
     status: statusCode,
     body: statusMessage
   })
 }
+
+// res.header('Access-Control-Allow-Origin', '*')
+// res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
+// res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+// res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE')
