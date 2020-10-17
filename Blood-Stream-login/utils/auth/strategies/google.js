ad613b2405fe664f49180490b4cae218f82b1198
cargdev <ingecarlos.gutierrez@gmail.com>
2020-10-17 03:27:11 -0500

    Adding google

---
+++ Blood-Stream-login/utils/auth/strategies/google.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const passport = require('passport')
+const { OAuth2Strategy: GoogleStrategy } = require('passport-google-oauth')
+const axios = require('axios')
+const boom = require('@hapi/boom')
+const config = require('../../../../config/config')
+const { method } = require('lodash')
+
+passport.use(
+  new GoogleStrategy({
+    clientID: config(false).googleClientId,
+    clientSecret: config(false).googleClientSecret,
+    callbackURL: 'auth/google/callback'
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    const { data, status } = await axios({
+      url: `${config(false).apiUrl}/api/auth/sign-provider`,
+      method: 'post',
+      data: {
+        name: profile.name,
+        email: profile.email,
+        password: profile.id,
+        apiKeyToken: config(false).apiKeyToken
+      }
+    })
+
+    if (!data || status !== 200) return cb(boom.unauthorized(), false)
+
+    return cb(null, data)
+  }
+  )
+)
--- config/config.js
+++ config/config.js
@@ -8,6 +8,16 @@ module.exports = function config (configExtra) {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     host: process.env.DB_HOST,
+    googleClientId: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    sessionSecret: process.env.SESSION_SECRET,
+    twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY,
+    twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
+    defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
+    authJwtSecret: process.env.AUTH_JWT_SECRET,
+    apiUrl: process.env.API_URL,
+    apiKeyToken: process.env.API_KEY_TOKEN,
     dialect: 'postgres',
     logging: s => debug(s)
   }
